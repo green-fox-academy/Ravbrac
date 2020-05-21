19103b615445a50dff4a9d47b674e2449f371e83
Ravbrac <soltadamx@gmail.com>
2020-05-21 15:01:33 +0200

    Done IntroduceYourself exercise

---
+++ week-01/day-4/src/introduce_yourself/IntroduceYourself.java
@@ -0,0 +1,9 @@
+package introduce_yourself;
+
+public class IntroduceYourself {
+    public static void main(String[] args) {
+        System.out.println("Adam Soltesz");
+        System.out.println(32);
+        System.out.println(1.68);
+    }
+}
