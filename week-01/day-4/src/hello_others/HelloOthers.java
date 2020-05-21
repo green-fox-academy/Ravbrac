166ee3f12a26d6224c44c67f115fcc14898937e7
Ravbrac <soltadamx@gmail.com>
2020-05-21 14:50:25 +0200

    Done HelloOthers exercise

---
+++ week-01/day-4/src/hello_others/HelloOthers.java
@@ -0,0 +1,9 @@
+package hello_others;
+
+public class HelloOthers {
+    public static void main(String[] args) {
+        System.out.println("Hello, Zaho!");
+        System.out.println("Hello, Viki!");
+        System.out.println("Hello, Dani!");
+    }
+}
