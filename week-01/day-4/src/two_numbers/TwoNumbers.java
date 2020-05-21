bf4aa8435b9fd51a9d74063988a164b48da99f52
Ravbrac <soltadamx@gmail.com>
2020-05-21 15:22:28 +0200

    Done TwoNumbers exercise

---
+++ week-01/day-4/src/two_numbers/TwoNumbers.java
@@ -0,0 +1,22 @@
+package two_numbers;
+
+
+public class TwoNumbers {
+    public static void main(String[] args) {
+        int a = 22;
+        int b = 13;
+
+        System.out.println(b + a);
+
+        System.out.println(a - b);
+
+        System.out.println(a * b);
+
+        System.out.println((float) a / b);
+
+        System.out.println(a / b);
+
+        System.out.println(a % b);
+
+    }
+}
