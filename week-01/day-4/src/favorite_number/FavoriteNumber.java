a88c3566b55cdc60afd812efdf21047ee770a051
Ravbrac <soltadamx@gmail.com>
2020-05-21 15:49:24 +0200

    Done FavoriteNumber

---
+++ week-01/day-4/src/favorite_number/FavoriteNumber.java
@@ -0,0 +1,9 @@
+package favorite_number;
+
+public class FavoriteNumber {
+    public static void main(String[] args) {
+        int favoriteNumber = 13;
+
+        System.out.println("My favorite number is : " + favoriteNumber);
+    }
+}
