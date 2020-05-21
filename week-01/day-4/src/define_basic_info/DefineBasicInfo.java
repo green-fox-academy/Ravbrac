0c23f6184b6516175cdbd38f5a1af13c4f3d4f23
Ravbrac <soltadamx@gmail.com>
2020-05-21 16:25:49 +0200

    Done DefineBasicInfo

---
+++ week-01/day-4/src/define_basic_info/DefineBasicInfo.java
@@ -0,0 +1,15 @@
+package define_basic_info;
+
+public class DefineBasicInfo {
+    public static void main(String[] args) {
+       String name = "Adam Soltesz";
+       int age = 32;
+       double height = 1.68;
+       boolean married = false;
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(married);
+    }
+}
