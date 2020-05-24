a735d32303fc53b15d09147b637dc632adfe4266
Ravbrac <soltadamx@gmail.com>
2020-05-24 14:26:51 +0200

    Done DrawSquare

---
+++ week-01/day-5/src/draw_square/DrawSquare.java
@@ -0,0 +1,46 @@
+package draw_square;
+
+import java.util.*;
+
+public class DrawSquare {
+    public static void main(String[] args) {
+        // Write a program that reads a number from the standard input, then draws a
+        // square like this:
+        //
+        //
+        // %%%%%%
+        // %    %
+        // %    %
+        // %    %
+        // %    %
+        // %%%%%%
+        //
+        // The square should have as many lines as the number was
+
+        Scanner input = new Scanner (System.in);
+        System.out.println("Enter the amount: ");
+        int amount = input.nextInt();
+
+        for (int i = 0; i < amount; i++) {
+            System.out.print("%");
+        }
+
+        System.out.println("");
+
+        for (int i = 0; i < amount-2; i++) {
+            for (int j = 0; j < amount; j++) {
+                if (j == 0 || j == amount - 1) {
+                    System.out.print("%");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+
+            System.out.println("");
+        }
+
+        for (int i = 0; i < amount; i++) {
+            System.out.print("%");
+        }
+    }
+}
