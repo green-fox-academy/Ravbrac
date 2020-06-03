f858806383b195b5a607432876fd18c451f4d3a6
Ravbrac <soltadamx@gmail.com>
2020-06-03 14:48:07 +0200

    Done PostIt

---
+++ week-03/day-3-4/src/post_it/Main.java
@@ -0,0 +1,9 @@
+package post_it;
+
+public class Main {
+    public static void main(String[] args) {
+        PostIt postIdea1 = new PostIt("orange", "blue", "Idea 1");
+        PostIt postAwesome = new PostIt("pink", "black", "Awesome");
+        PostIt postSuperb = new PostIt("yellow", "green", "Superb!");
+    }
+}
---
+++ week-03/day-3-4/src/post_it/PostIt.java
@@ -0,0 +1,24 @@
+package post_it;
+
+public class PostIt {
+//    Create a PostIt class that has
+//            a backgroundColor
+//            a text on it
+//            a textColor
+//            Create a few example post-it objects:
+//    an orange with blue text: "Idea 1"
+//    a pink with black text: "Awesome"
+//    a yellow with green text: "Superb!"
+
+    private String backgroundColor;
+    private String text;
+    private String  textColor;
+
+    public PostIt(String backgroundColor, String text, String textColor) {
+        this.backgroundColor = backgroundColor;
+        this.text = text;
+        this.textColor = textColor;
+    }
+}
+
+
