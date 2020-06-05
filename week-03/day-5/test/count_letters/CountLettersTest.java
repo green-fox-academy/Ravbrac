a08b272857dab75095bcd0b8ef61549e64a44551
Ravbrac <soltadamx@gmail.com>
2020-06-05 17:27:52 +0200

    Updated CountLetters test

--- week-03/day-5/test/count_letters/CountLettersTest.java
+++ week-03/day-5/test/count_letters/CountLettersTest.java
@@ -22,4 +22,33 @@ public class CountLettersTest {
         assertEquals(testMap, counter.letterCounter(word));
 
     }
+
+    @Test
+    public void wrongValue() {
+        CountLetters counter = new CountLetters();
+        String word = "grammar";
+
+        HashMap<Character, Integer> testMap = new HashMap<>();
+        testMap.put(word.toCharArray()[0], 1);
+        testMap.put(word.toCharArray()[1], 1);
+        testMap.put(word.toCharArray()[2], 1);
+        testMap.put(word.toCharArray()[3], 1);
+
+        assertNotEquals(testMap, counter.letterCounter(word));
+    }
+
+    @Test
+    public void wrongKey() {
+        CountLetters counter = new CountLetters();
+        String word = "grammar";
+        String wrongWord = "qwerqw";
+
+        HashMap<Character, Integer> testMap = new HashMap<>();
+        testMap.put(wrongWord.toCharArray()[0], 1);
+        testMap.put(wrongWord.toCharArray()[1], 2);
+        testMap.put(wrongWord.toCharArray()[2], 2);
+        testMap.put(wrongWord.toCharArray()[3], 2);
+
+        assertNotEquals(testMap, counter.letterCounter(word));
+    }
 }
