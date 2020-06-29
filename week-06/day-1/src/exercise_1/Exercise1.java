fd28a52b71d1532e8947cd90a9760fc30c4b7de5
Ravbrac <soltadamx@gmail.com>
2020-06-29 13:32:24 +0200

    Done Exercise 2

--- week-06/day-1/src/exercise_1/Exercise1.java
+++ week-06/day-1/src/exercise_1/Exercise1.java
@@ -3,7 +3,7 @@ package exercise_1;
 import java.util.Arrays;
 import java.util.List;
 
-public class exercise1 {
+public class Exercise1 {
   public static void main(String[] args) {
     List<Integer> numbers = Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14);
 
