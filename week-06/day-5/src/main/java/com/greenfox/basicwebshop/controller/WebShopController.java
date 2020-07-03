4d4430d7509959f7fcb4e203a263745775e203b1
Ravbrac <soltadamx@gmail.com>
2020-07-03 12:55:42 +0200

    Done Navigation & Header

--- week-06/day-5/src/main/java/com/greenfox/basicwebshop/controller/WebShopController.java
+++ week-06/day-5/src/main/java/com/greenfox/basicwebshop/controller/WebShopController.java
@@ -6,9 +6,14 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class WebShopController {
+//  @GetMapping("/webshop")
+//  @ResponseBody
+//  public String helloWorld() {
+//    return "Hello World";
+//  }
+
   @GetMapping("/webshop")
-  @ResponseBody
-  public String helloWorld() {
-    return "Hello World";
+  public String webshop() {
+    return "webshop";
   }
 }
