516704fd8a8958284ce72829b7305eb21c1ea927
Ravbrac <soltadamx@gmail.com>
2020-07-09 14:51:04 +0200

    Added login page

--- week-07/day-4-5/src/main/java/com/greenfox/foxclub/controller/MainController.java
+++ week-07/day-4-5/src/main/java/com/greenfox/foxclub/controller/MainController.java
@@ -1,12 +1,49 @@
 package com.greenfox.foxclub.controller;
 
+import com.greenfox.foxclub.model.Fox;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
+    Fox fox = new Fox();
+
+//    @GetMapping("/")
+//    public String sendToLogin() {
+//        return "redirect:/login";
+//    }
+
+//    @GetMapping("/")
+//    public String index(Model model) {
+//        model.addAttribute("name", fox.getName());
+//
+//        return "index";
+//    }
+
     @GetMapping("/")
-    public String indexController() {
-    return "index";
-}
+    public String loggedInIndex(@RequestParam(defaultValue = "empty") String name, Model model) {
+        if (name.equals("empty")) {
+            return "login";
+        }else {
+            model.addAttribute("name", name);
+
+            return "index";
+        }
+
+    }
+
+    @GetMapping("/login")
+    public String getLogin() {
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String postLogin(@RequestParam String name) {
+        String s = "?name=" + name;
+
+        return "redirect:/" + s;
+    }
 }
---
+++ week-07/day-4-5/src/main/java/com/greenfox/foxclub/model/Fox.java
@@ -0,0 +1,17 @@
+package com.greenfox.foxclub.model;
+
+public class Fox {
+    private String name;
+
+    public Fox() {
+        this.name = "Mr. Fox";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
