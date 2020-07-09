77b09170646405e1da5a8dd442e512bb6528f432
Ravbrac <soltadamx@gmail.com>
2020-07-09 21:44:35 +0200

    Added information page

--- week-07/day-4-5/src/main/java/com/greenfox/foxclub/controller/MainController.java
+++ week-07/day-4-5/src/main/java/com/greenfox/foxclub/controller/MainController.java
@@ -7,9 +7,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 @Controller
 public class MainController {
-    Fox fox = new Fox();
+    List<Fox> foxes = new ArrayList<>();
 
 //    @GetMapping("/")
 //    public String sendToLogin() {
@@ -25,14 +29,22 @@ public class MainController {
 
     @GetMapping("/")
     public String loggedInIndex(@RequestParam(defaultValue = "empty") String name, Model model) {
-        if (name.equals("empty")) {
+        if (name.equals("empty") || !findFox(name).isPresent()) {
             return "login";
         }else {
-            model.addAttribute("name", name);
+            Fox fox = findFox(name).get();
+            model.addAttribute("name", fox.getName());
+            model.addAttribute("food", fox.getFood());
+            model.addAttribute("drink", fox.getDrink());
+            model.addAttribute("tricks", fox.getTricks());
+            if (fox.getTricks().isEmpty()) {
+                model.addAttribute("noTrick", "You know no tricks, yet. Go and learn some.");
+            } else {
+                model.addAttribute("noTrick", "");
+            }
 
             return "index";
         }
-
     }
 
     @GetMapping("/login")
@@ -42,8 +54,15 @@ public class MainController {
 
     @PostMapping("/login")
     public String postLogin(@RequestParam String name) {
+        foxes.add(new Fox(name));
         String s = "?name=" + name;
 
         return "redirect:/" + s;
+    }
+
+    public  Optional<Fox> findFox(String name) {
+        return foxes.stream()
+                .filter(f -> f.getName().equals(name))
+                .findFirst();
     }
 }
--- week-07/day-4-5/src/main/java/com/greenfox/foxclub/model/Fox.java
+++ week-07/day-4-5/src/main/java/com/greenfox/foxclub/model/Fox.java
@@ -1,10 +1,20 @@
 package com.greenfox.foxclub.model;
 
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Fox {
     private String name;
+    private List<String> tricks;
+    private String food;
+    private String drink;
 
-    public Fox() {
-        this.name = "Mr. Fox";
+    public Fox(String name) {
+        this.name = name;
+        this.tricks = new ArrayList<>();
+        this.food = "pizza";
+        this.drink = "cola";
     }
 
     public String getName() {
@@ -13,5 +23,29 @@ public class Fox {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public List<String> getTricks() {
+        return tricks;
+    }
+
+    public void setTricks(List<String> tricks) {
+        this.tricks = tricks;
+    }
+
+    public String getFood() {
+        return food;
+    }
+
+    public void setFood(String food) {
+        this.food = food;
+    }
+
+    public String getDrink() {
+        return drink;
+    }
+
+    public void setDrink(String drink) {
+        this.drink = drink;
     }
 }
